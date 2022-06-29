a9a1c44b1bdef66b718db604caa94a9eb811c7d9
David Borja <DBorja95@administrators-iMac.local>
2022-06-29 08:38:09 -0600

    fetch works

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// unsplash API
+const count = 10;
+const apiKey = "CV7q5kYIV8y6uEczGsS_tR3nnblf7cOas08DTjf38RM";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// get photos from unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // catch error here
+  }
+}
+
+// on load
+getPhotos();
