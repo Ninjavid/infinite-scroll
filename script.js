0a9b9d72b193a0f026a35a55ebaf700869c61208
David Borja <DBorja95@administrators-iMac.local>
2022-06-29 12:21:53 -0600

    fixed

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 
 // Create elements for links and photos add to DOM
 function displayPhotos() {
-  // Run function for each object in photosArray
+  // run function for eachc object in photos array
   photosArray.forEach((photo) => {
-    // create <a> to link to Unsplash
+    // create <a> to link to unsplash
     const item = document.createElement("a");
     item.setAttribute("href", photo.links.html);
     item.setAttribute("target", "_blank");
     // create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("src, photo.urls.regular");
     img.setAttribute("alt", photo.alt_description);
     img.setAttribute("title", photo.alt_description);
-    // put <img> inside <a> then put both inside imageContainer element
+    // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -32,7 +32,7 @@ async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    displayPhotos;
+    displayPhotos();
   } catch (error) {
     // catch error here
   }
